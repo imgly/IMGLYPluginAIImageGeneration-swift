a67ccd07a6870a79dab23e940b089e505ebc038e
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-08-01 00:02:41 +0000

    IMGLYPluginAIImageGeneration version 1.80.0-rc.0

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     .library(name: "IMGLYPluginAIImageGeneration", targets: ["IMGLYPluginAIImageGeneration"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/imgly/IMGLYUI-swift.git", exact: "1.79.0"),
+    .package(url: "https://github.com/imgly/IMGLYUI-swift.git", exact: "1.80.0-rc.0"),
   ],
   targets: [
     .target(
